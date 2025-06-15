471b45653a829b8fcb2254f484c5dab9115253d5
Tabrej Ansari <contactwithtabrej@gmail.com>
2025-06-15 21:29:14 +0545

    responsiveness work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,24 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          {/* MailchimpForm - centered on mobile */}
+          <Col xs={12} className="text-center mb-4 mb-md-0">
+            <MailchimpForm />
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/tabrej-ansari/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://x.com/KhanTbrz"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/itstbrz/"><img src={navIcon3} alt="Icon" /></a>
+          
+          {/* Logo - centered on mobile */}
+          <Col xs={12} sm={6} className="text-center mb-4 mb-sm-0">
+            <img src={logo} alt="Logo" style={{ maxWidth: '200px' }} />
+          </Col>
+          
+          {/* Social icons and text - centered on mobile */}
+          <Col xs={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon d-flex justify-content-center justify-content-sm-end mb-3">
+              <a href="https://www.linkedin.com/in/tabrej-ansari/"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="https://x.com/KhanTbrz"><img src={navIcon2} alt="Twitter" /></a>
+              <a href="https://www.instagram.com/itstbrz/"><img src={navIcon3} alt="Instagram" /></a>
             </div>
-            <p>Handcrafted with ❤️ by Tabrej | © 2025 All rights reserved.</p>
+            <p className="text-center text-sm-end">Handcrafted with ❤️ by Tabrej | © 2025 All rights reserved.</p>
           </Col>
         </Row>
       </Container>
